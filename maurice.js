c76d888da252f620f3890644711bfd3fde34d78c
ipk <ipk1@pm.me>
2018-11-09 01:24:39 -0600

    update style and js

--- maurice.js
+++ maurice.js
@@ -2,10 +2,13 @@ require([
   "esri/Map",
   "esri/views/MapView",
 /*require basemap gallery widget to load with page*/
-  "esri/widgets/BasemapGallery"
+  "esri/widgets/BasemapGallery",
+  "esri/widgets/BasemapToggle",
+  "dojo/parser",
+  "dojo/domReady!"
 ],
 /*below are required to run*/
-function(Map, MapView, BasemapGallery){
+function(Map, MapView, BasemapGallery, BasemapToggle){
 /*creates a new map variable and defines the basemap style and additional layers*/
   var map = new Map({
     basemap: "dark-gray",
@@ -17,15 +20,36 @@ function(Map, MapView, BasemapGallery){
     zoom: 12,
     center: [-97.775462, 30.270076]
     });
+
+    var toggle = new BasemapToggle({
+      titleVisible: true,
+
+              // 2 - Set properties
+      view: view, // view that provides access to the map's 'topo' basemap
+      nextBasemap: "hybrid", // allows for toggling to the 'hybrid' basemap
+      });
+      // 3 - Call startup on the widget
+      toggle.startup();
+
+      // Add the BasemapToggle widget to the top right corner of the view
+      view.ui.add(toggle, "bottom-left");
+
+
+  /*var basemapToggle = new BasemapToggle({
+  view: view,  // The view that provides access to the map's "streets" basemap
+  nextBasemap: "satellite"  // Allows for toggling to the "hybrid" basemap
+});*/
 /*creates a variable to store new BasemapGallery and defines which view to add it to*/
-  var basemapGallery = new BasemapGallery({
+  /*var basemapGallery = new BasemapGallery({
+    showArcGISBasemaps: true,
     map: map
   }, "basemapTab");
+  basemapGallery.startup();
 /*adds a new ui element to our existing view and defines its position at the top right of the page*/
-  /*view.ui.add(basemapGallery, {
+  /*view.ui.add(basemapToggle, {
     position: "top-left"
   });*/
 
-  view.ui.move("zoom", "bottom-left");		//Move Zoom to bottom left
+  view.ui.move("zoom", "top-left");		//Move Zoom to top left
 
 });
