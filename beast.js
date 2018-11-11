6cbca36aa89e11c73541bbaf5039a04a1c72a840
ipk <ipk1@pm.me>
2018-11-11 00:12:05 -0600

    update toolbox and mobile site

--- beast.js
+++ beast.js
@@ -18,31 +18,50 @@ function projectOverlay() {
   document.getElementById("overlayTeam").style.display = "none";
 }
 
-/*function openMenu(titleMenu) {
-  var i;
-  var x = document.getElementsByClassName("title");
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  document.getElementById(titleMenu).style.display = "block";
-}*/
-
 function openMenu(evt, titleMenu) {
   var i, x, tablinks;
   x = document.getElementsByClassName("title");
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+    x[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-     tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+    tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
   }
   document.getElementById(titleMenu).style.display = "block";
   evt.currentTarget.firstElementChild.className += " w3-border-red";
   document.getElementById("boxContent").style.display = "block";
 }
 
-function closeMenu() {
+function closeMenu(evt, titleMenu) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("title");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+  }
+  document.getElementsByClassName("tablink") - " w3-border-red";
   document.getElementById("boxContent").style.display = "none";
-  document.getElementsByClassName("tablink").style.display = "none";
+  document.getElementById("boxMenu").style.display = "none";
+}
+
+function showMenu() {
+  document.getElementById("boxMenu").style.display = "block";
+}
+
+function touchMenu() {
+  var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight || e.clientHeight || g.clientHeight;
+
+  if (x <= 450) {
+    document.getElementById("touchMenu").innerHTML = "&#9776;";
+    document.getElementById("boxMenu").style.display = "none";
+  }
 }
