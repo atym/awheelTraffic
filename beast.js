fed45c0858a4bdff5429824bf6d46ad343b123a0
ipk <ipk1@pm.me>
2018-11-10 22:07:04 -0600

    update toolbox style

--- beast.js
+++ beast.js
@@ -17,3 +17,32 @@ function projectOverlay() {
   document.getElementById("overlayProject").style.display = "block";
   document.getElementById("overlayTeam").style.display = "none";
 }
+
+/*function openMenu(titleMenu) {
+  var i;
+  var x = document.getElementsByClassName("title");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  document.getElementById(titleMenu).style.display = "block";
+}*/
+
+function openMenu(evt, titleMenu) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("title");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+  }
+  document.getElementById(titleMenu).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-border-red";
+  document.getElementById("boxContent").style.display = "block";
+}
+
+function closeMenu() {
+  document.getElementById("boxContent").style.display = "none";
+  document.getElementsByClassName("tablink").style.display = "none";
+}
--- maurice.js
+++ maurice.js
@@ -1,43 +1,43 @@
 require([
-  "esri/Map",
-  "esri/views/MapView",
-/*require basemap gallery widget to load with page*/
-  "esri/widgets/BasemapGallery",
-  "esri/widgets/BasemapToggle",
-  "dojo/parser",
-  "dojo/domReady!"
-],
-/*below are required to run*/
-function(Map, MapView, BasemapGallery, BasemapToggle){
-/*creates a new map variable and defines the basemap style and additional layers*/
-  var map = new Map({
-    basemap: "dark-gray",
+    "esri/Map",
+    "esri/views/MapView",
+    /*require basemap gallery widget to load with page*/
+    "esri/widgets/BasemapGallery",
+    "esri/widgets/BasemapToggle",
+    "dojo/parser",
+    "dojo/domReady!"
+  ],
+    /*below are required to run*/
+  function(Map, MapView, BasemapGallery, BasemapToggle) {
+    /*creates a new map variable and defines the basemap style and additional layers*/
+    var map = new Map({
+      basemap: "dark-gray",
     });
-/*creates a new view and assigns it to div with specified ID, sets default parameters including where the map will be centered on load*/
-  var view = new MapView({
-    container: "viewDiv",
-    map: map,
-    zoom: 12,
-    center: [-97.775462, 30.270076]
+    /*creates a new view and assigns it to div with specified ID, sets default parameters including where the map will be centered on load*/
+    var view = new MapView({
+      container: "viewDiv",
+      map: map,
+      zoom: 12,
+      center: [-97.775462, 30.270076]
     });
 
     var toggle = new BasemapToggle({
       titleVisible: true,
       view: view,
       nextBasemap: "hybrid",
-      });
-      view.ui.add(toggle, "bottom-right");
+    });
+    view.ui.add(toggle, "bottom-right");
 
-/*creates a variable to store new BasemapGallery and defines which view to add it to*/
-  /*var basemapGallery = new BasemapGallery({
+    /*creates a variable to store new BasemapGallery and defines which view to add it to*/
+    /*var basemapGallery = new BasemapGallery({
     showArcGISBasemaps: true,
     map: map
-  }, "basemapTab");
-  basemapGallery.startup();
-/*adds a new ui element to our existing view and defines its position at the top right of the page*/
-  /*view.ui.add(basemapToggle, {
-    position: "top-left"
-  });*/
+    }, "basemapTab");
+    basemapGallery.startup();
+    /*adds a new ui element to our existing view and defines its position at the top right of the page*/
+    /*view.ui.add(basemapToggle, {
+      position: "top-left"
+    });*/
 
-  view.ui.move("zoom", "bottom-right");		//Move Zoom to top left
-});
+    view.ui.move("zoom", "bottom-right"); //Move Zoom to top left
+  });
