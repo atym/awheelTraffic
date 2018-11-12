2c49ab11a96ae69b23be9ae83c1719c7a6a081cf
ipk <ipk1@pm.me>
2018-11-12 10:53:10 -0600

    clean beast
    I deleted unnecessary functions and added comments to blocks of code.

--- beast.js
+++ beast.js
@@ -1,4 +1,17 @@
-var activeMenu = false;
+/**************************************************
+ * WELCOME to Beast
+ **************************************************/
+/**************************************************
+ * AUSTIN TRAFFIC INCIDENTS
+ * by
+ * Jason Bartling
+ * Ian Kelly
+ * Brandon Rose
+ **************************************************/
+
+/**************************************************
+ * Open and Close full screen overlay on logo click
+ **************************************************/
 
 function openOverlay() {
   document.getElementById("fullOverlay").style.width = "100%";
@@ -10,6 +23,10 @@ function closeOverlay() {
   document.getElementById("fullOverlay").style.width = "0%";
 }
 
+/**************************************************
+ * Switch between team info and project info
+ **************************************************/
+
 function teamOverlay() {
   document.getElementById("overlayTeam").style.display = "block";
   document.getElementById("overlayProject").style.display = "none";
@@ -20,49 +37,27 @@ function projectOverlay() {
   document.getElementById("overlayTeam").style.display = "none";
 }
 
+/**************************************************
+ * Checkbox functionalities
+ * City Limits and Road Network
+ **************************************************/
+
 function validate() {
-    if (document.getElementById('setBoundary').checked) {
-        alert("checked");
-    } else {
-        alert("You didn't check it! Let me check it for you.");
-    }
+  if (document.getElementById('setBoundary').checked) {
+    alert("checked");
+  } else {
+    alert("You didn't check it! Let me check it for you.");
+  }
 }
 
-function closeTap(tablinks, evt, titleMenu, activeMenu) {
-  var div1 = document.getElementById("boxContent");
-  var event = evt;
+/**************************************************
+ * Show boxContent when menu bar button clicked
+ * Define button style (selected)
+ **************************************************/
 
-  console.log(event);
-  console.log(activeMenu);
-
-    if (activeMenu == true) {
-      console.log("Hello World");
-      document.getElementById("boxContent").style.display = "none";
-      activeMenu = false;
-      console.log(activeMenu);
-
-    }
-
-
-
-  /*if (div1.style.display == 'block') {
-      console.log(tablinks);
-      console.log(titleMenu);
-      console.log(evt);
-  }*/
-
-  /*else {
-        document.getElementById("boxMenu").style.display = 'block';
-
-}*/
-
-}
-
-function openMenu(evt, titleMenu, activeMenu) {
+function openMenu(evt, titleMenu) {
   var i, x, tablinks;
   x = document.getElementsByClassName("title");
-  activeMenu = true;
-  closeTap(tablinks, evt, titleMenu, activeMenu);
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
@@ -73,9 +68,11 @@ function openMenu(evt, titleMenu, activeMenu) {
   document.getElementById(titleMenu).style.display = "block";
   evt.currentTarget.firstElementChild.className += " w3-border-red";
   document.getElementById("boxContent").style.display = "block";
-
-
 }
+
+/**************************************************
+ * Close boxContent on carot click
+ **************************************************/
 
 function closeMenu(evt, titleMenu) {
   var i, x, tablinks;
@@ -83,54 +80,5 @@ function closeMenu(evt, titleMenu) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  /*tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
-  }
-  document.getElementsByClassName("tablink") - " w3-border-red";*/
   document.getElementById("boxContent").style.display = "none";
-}
-
-/*function closeMenu2() {
-  var menu = document.getElementById("touchMenu");
-  var menuContent = menu.innerHTML;
-  console.log(menuContent);
-  if (menuContent = "&#9776;") {
-    document.getElementById("boxMenu").style.display = "none";
-  } else {
-    document.getElementById("boxMenu").style.display = "block";
-  }
-}*/
-
-function showMenu() {
-  var div1 = document.getElementById("boxMenu");
-  var div2 = document.getElementById("boxContent");
-  if (div1.style.display == 'none') {
-    div1.style.display = 'block'
-  } else {
-    closeMenu();
-    document.getElementById("boxMenu").style.display = 'none';
-
-  }
-}
-
-function touchMenu() {
-  var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight || e.clientHeight || g.clientHeight;
-
-  /*var z = document.getElementsByClassName("w3-third tablink w3-bottombar w3-hover-light-grey w3-padding");*/
-
-  /*if (x <= 450) {
-
-    /*for (i = 0; i < z.length; i++) {
-      z[i].style.display = "none";
-    }*/
-  /*  console.log(z);
-    /*document.getElementById("touchMenu").innerHTML = "&#9776;";
-    document.getElementById("boxMenu").style.display = "none";
-  }*/
 }
