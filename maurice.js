893aa318c728018982496480e47a85d5ab35d2bb
ipk <ipk1@pm.me>
2018-11-12 00:51:12 -0600

    clean maurice

--- maurice.js
+++ maurice.js
@@ -1,21 +1,43 @@
+/**************************************************
+ * WELCOME to maurice.js
+ **************************************************/
+/**************************************************
+ * AUSTIN TRAFFIC INCIDENTS
+ * by
+ * Jason Bartling
+ * Ian Kelly
+ * Brandon Rose
+ **************************************************/
+
 require([
     "esri/Map",
     "esri/views/MapView",
-    /*require basemap gallery widget to load with page*/
-    "esri/widgets/BasemapGallery",
     "esri/widgets/BasemapToggle",
     "esri/layers/FeatureLayer",
     "esri/geometry/Point",
     "esri/request"
   ],
-  /*below are required to run*/
-  function(Map, MapView, BasemapGallery, BasemapToggle, FeatureLayer, Point, esriRequest) {
+
+  /**************************************************
+   * Create magic mapping function
+   **************************************************/
+
+  function(Map, MapView, BasemapToggle, FeatureLayer, Point, esriRequest) {
+
+    /**************************************************
+     * VARIABLES
+     **************************************************/
+
+    var limits, roads, trafficFLayer, fields, pTemplate, trafficRenderer, map, view, json;
+
+    /**************************************************
+     * Create feature for city limits boundary
+     **************************************************/
 
     var limits = new FeatureLayer({
-      url: "https://services9.arcgis.com/E9UVIqvAicEqTOkL/arcgis/rest/services/acl2018/FeatureServer" /*url to the city boundary ArcGIS service*/
-
+      url: "https://services9.arcgis.com/E9UVIqvAicEqTOkL/arcgis/rest/services/acl2018/FeatureServer"
     });
-    var trafficFLayer;
+
     /**************************************************
      * Define the specification for each field to create
      * in the layer
@@ -54,10 +76,12 @@ require([
     /**************************************************
      * Define the popup for info about incidents
      **************************************************/
+
     var pTemplate = {
       title: "<strong>{issueReported}</strong>",
       content: "Location: {address}<br> Time: {statusDateTime}"
     };
+
     /**************************************************
      * Define the renderer for symbolizing incidents
      **************************************************/
@@ -71,12 +95,19 @@ require([
       }
     };
 
-    /*creates a new map variable and defines the basemap style and additional layers*/
+    /**************************************************
+     * Create map and define basemap
+     **************************************************/
+
     var map = new Map({
       basemap: "dark-gray",
       layers: [limits]
     });
-    /*creates a new view and assigns it to div with specified ID, sets default parameters including where the map will be centered on load*/
+
+    /**************************************************
+     * Assign map to it's HTML container, load extent
+     **************************************************/
+
     var view = new MapView({
       container: "viewDiv",
       map: map,
@@ -84,10 +115,17 @@ require([
       center: [-97.775462, 30.270076]
     });
 
-    //Once the layer loads, set the view's extent to the layer's fullextent
+    /**************************************************
+     * Once the limits layer loads, set the view's extent to the fullextent
+     **************************************************/
+
     limits.when(function() {
       view.extent = limits.fullExtent;
     });
+
+    /**************************************************
+     * Create basemap toggle and style it
+     **************************************************/
 
     var toggle = new BasemapToggle({
       titleVisible: true,
@@ -96,16 +134,24 @@ require([
     });
     view.ui.add(toggle, "bottom-right");
 
-    // Request the  data from data.austin when the view resolves
-    // then send it to the createGraphics() method
-    // when graphics are created, create the layer
+    /**************************************************
+     * Request the  data from data.austin when the
+     * view resolves then send it to the
+     * createGraphics() method when graphics are created,
+     * create the layer
+     **************************************************/
+
     view.when(function() {
 
       getData()
         .then(createGraphics)
         .then(createLayer);
     });
-    // Request the traffic incident data
+
+    /**************************************************
+     * Request traffic incident data
+     **************************************************/
+
     function getData() {
 
       var url = "https://data.austintexas.gov/resource/r3af-2r8x.json?$$app_token=EoIlIKmVmkrwWkHNv5TsgP1CM&traffic_report_status=ACTIVE";
@@ -160,17 +206,11 @@ require([
       return trafficFLayer;
     }
 
-    /*creates a variable to store new BasemapGallery and defines which view to add it to*/
-    /*var basemapGallery = new BasemapGallery({
-    showArcGISBasemaps: true,
-    map: map
-    }, "basemapTab");
-    basemapGallery.startup();
-    /*adds a new ui element to our existing view and defines its position at the top right of the page*/
-    /*view.ui.add(basemapToggle, {
-      position: "top-left"
-    });*/
+    /**************************************************
+     * Modify page elements
+     **************************************************/
 
     view.ui.move("zoom", "bottom-right"); //Move Zoom to top left
+
 
   });
