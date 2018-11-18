c6d85edbae510345e70f551104c180c037e7297b
ipk <ipk1@pm.me>
2018-11-17 22:54:19 -0600

    beautify

--- mapping.js
+++ mapping.js
@@ -26,8 +26,9 @@ require([
     "esri/widgets/Locate",
     "esri/request",
     "dojo/dom",
-	  "dojo/on",
-	  "esri/core/promiseUtils"],
+    "dojo/on",
+    "esri/core/promiseUtils"
+  ],
 
   /**************************************************
    * Create magic mapping function
@@ -35,7 +36,7 @@ require([
 
 
   function(Map, Basemap, MapView, BasemapToggle, FeatureLayer, VectorTileLayer, TileLayer, Point, Legend, Home, ScaleBar, Search, BaseTileLayer, Locate,
-		esriRequest, dom, on, promiseUtils) {
+    esriRequest, dom, on, promiseUtils) {
 
     /**************************************************
      * VARIABLES
@@ -43,7 +44,7 @@ require([
 
     var limits, roads, trafficFLayer, fields, pTemplate, trafficRenderer, map, view, legend, roadLayerToggle, cityLimitsLayerToggle, trafficRequestURL, baseToggle, lightRoads, darkRoads, vectorRoads, satelliteBase, satelliteReference, satellite, homeBtn, scaleBar, locateWidget, currentTraffic;;
     var json, recordsReturned;
-	
+
     /**************************************************
      * Create variables for vector layers
      * Combine layers into new basemap
@@ -319,12 +320,12 @@ require([
         .then(createGraphics)
         .then(createLayer)
         .then(createLegend);
-      
-	  // Populate the select list with all of the possible incident types JB
+
+      // Populate the select list with all of the possible incident types JB
       populateSearch();
-	  
-	  // Run the search once the submit button has been clicked JB
-	  on(dom.byId("submitButton"), "click", runSearch);
+
+      // Run the search once the submit button has been clicked JB
+      on(dom.byId("submitButton"), "click", runSearch);
     });
 
     /**************************************************
@@ -332,124 +333,122 @@ require([
      **************************************************/
 
     function getData(jsonURL) {
-        
+
       return esriRequest(jsonURL, {
         responseType: "json"
       });
-	  
-	   
+
+
     };
-	
-	/*********************************************************
-	*  Add query parameters to JSON request and redisplay map 
-	*
-	*  Function author:  JB
-	*********************************************************/
-	function runSearch(){
-		
-		//Get current date
-		var now = new Date();
-        
-		//Use current date to get current time in milliseconds
-		var today = now.getTime();
-        
-		var millisecondsInDay = 86400000;
-        
-		//Parameter for amount of days to subtract from current date
-		var days = dom.byId("daysFromDate").value;
-		
-		// Make sure a number has been entered for days to search by 
-		if (days > 0 == false){
-			alert ("You must enter a number for the days to search by.");
-			
-			return;
-		}
-		else if (days > 365){
-			alert ("Please enter a number that is 365 or less.");
-			return;
-		}
-		//var incidentTypes = dom.byId("incidentTypes").value;
-		
-		var incidentTypes = [];
-		var incidentTypesString = "(";
-		
-		// Get array of dom options properties for the select list 
-		var options = dom.byId("incidentTypes").options;
-		
-		// Make sure at least one incident type is selected
-		if (options.selectedIndex == -1){
-			alert ("You must select at least one incident type.");
-			
-			return;
-		}
-		
-		// Add all selected elements to the incidentTypes array
-		for (var i=0; i < options.length; i++){
-			opt = options[i];
-			
-			if (opt.selected){
-				incidentTypes.push(opt.value);
-			}
-		}
-		
-		// Populate set of incidents with commas between them for the SQL statement
-		for (var i=0; i < incidentTypes.length; i++){
-			if (i < incidentTypes.length - 1){
-				incidentTypesString += "'" + incidentTypes[i] + "'" + ",";
-			}
-			else {
-				incidentTypesString += "'" + incidentTypes[i] + "'";
-			}
-		}
-		
-		//Close set of incidents
-		incidentTypesString += ")";
-		
-		//Multiply the number of days by milliseconds to get time in milliseconds to subtract from current time
-		var timeoffset = days * millisecondsInDay;
 
-		var queryTime = today - timeoffset;
-        
-		//Create new date object with adjusted date for query using time in milliseconds
-		var queryDate = new Date(queryTime);
-        
-		//Get different date parts to form query date string 
-		var queryDateYYYY = queryDate.getFullYear();
+    /*********************************************************
+     *  Add query parameters to JSON request and redisplay map
+     *
+     *  Function author:  JB
+     *********************************************************/
+    function runSearch() {
 
-		var queryDateMM = queryDate.getMonth() + 1;
+      //Get current date
+      var now = new Date();
 
-		var queryDateDD = queryDate.getDate();
+      //Use current date to get current time in milliseconds
+      var today = now.getTime();
 
-		var queryDateString = queryDateYYYY + "-" + queryDateMM + "-" + queryDateDD;
-		
-		/*var searchURL = "https://data.austintexas.gov/resource/r3af-2r8x.json" +
-		"?$where=traffic_report_status_date_time>"+"'"+queryDateString+"'"+
-		" AND issue_reported="+"'"+incidentTypes+"'"+
-		"&$$app_token=EoIlIKmVmkrwWkHNv5TsgP1CM";*/
-		
-		var searchURL = "https://data.austintexas.gov/resource/r3af-2r8x.json" +
-		"?$where=traffic_report_status_date_time>"+"'"+queryDateString+"'"+
-		" AND issue_reported IN "+ incidentTypesString +
-		"&$$app_token=EoIlIKmVmkrwWkHNv5TsgP1CM";
-		
-		console.log("Query params: "+queryDateString+" "+incidentTypes);
-		
-		//Remove the previous trafficFLayer before attempting to display the query results 
-		map.remove(trafficFLayer);
-		
-	    getData(searchURL)
+      var millisecondsInDay = 86400000;
+
+      //Parameter for amount of days to subtract from current date
+      var days = dom.byId("daysFromDate").value;
+
+      // Make sure a number has been entered for days to search by
+      if (days > 0 == false) {
+        alert("You must enter a number for the days to search by.");
+
+        return;
+      } else if (days > 365) {
+        alert("Please enter a number that is 365 or less.");
+        return;
+      }
+      //var incidentTypes = dom.byId("incidentTypes").value;
+
+      var incidentTypes = [];
+      var incidentTypesString = "(";
+
+      // Get array of dom options properties for the select list
+      var options = dom.byId("incidentTypes").options;
+
+      // Make sure at least one incident type is selected
+      if (options.selectedIndex == -1) {
+        alert("You must select at least one incident type.");
+
+        return;
+      }
+
+      // Add all selected elements to the incidentTypes array
+      for (var i = 0; i < options.length; i++) {
+        opt = options[i];
+
+        if (opt.selected) {
+          incidentTypes.push(opt.value);
+        }
+      }
+
+      // Populate set of incidents with commas between them for the SQL statement
+      for (var i = 0; i < incidentTypes.length; i++) {
+        if (i < incidentTypes.length - 1) {
+          incidentTypesString += "'" + incidentTypes[i] + "'" + ",";
+        } else {
+          incidentTypesString += "'" + incidentTypes[i] + "'";
+        }
+      }
+
+      //Close set of incidents
+      incidentTypesString += ")";
+
+      //Multiply the number of days by milliseconds to get time in milliseconds to subtract from current time
+      var timeoffset = days * millisecondsInDay;
+
+      var queryTime = today - timeoffset;
+
+      //Create new date object with adjusted date for query using time in milliseconds
+      var queryDate = new Date(queryTime);
+
+      //Get different date parts to form query date string
+      var queryDateYYYY = queryDate.getFullYear();
+
+      var queryDateMM = queryDate.getMonth() + 1;
+
+      var queryDateDD = queryDate.getDate();
+
+      var queryDateString = queryDateYYYY + "-" + queryDateMM + "-" + queryDateDD;
+
+      /*var searchURL = "https://data.austintexas.gov/resource/r3af-2r8x.json" +
+      "?$where=traffic_report_status_date_time>"+"'"+queryDateString+"'"+
+      " AND issue_reported="+"'"+incidentTypes+"'"+
+      "&$$app_token=EoIlIKmVmkrwWkHNv5TsgP1CM";*/
+
+      var searchURL = "https://data.austintexas.gov/resource/r3af-2r8x.json" +
+        "?$where=traffic_report_status_date_time>" + "'" + queryDateString + "'" +
+        " AND issue_reported IN " + incidentTypesString +
+        "&$$app_token=EoIlIKmVmkrwWkHNv5TsgP1CM";
+
+      console.log("Query params: " + queryDateString + " " + incidentTypes);
+
+      //Remove the previous trafficFLayer before attempting to display the query results
+      map.remove(trafficFLayer);
+
+      getData(searchURL)
         .then(createGraphics)
         .then(createLayer)
         .then(createLegend)
-		//Catch any of the errors that were created from the previous callback functions 
-		.catch(function(error){ 
-			console.log('One of the promises in the chain was rejected! Message: ', error);
-		});
-		
-		// Display the amount of results returned
-		//recordsReturned = Object.keys(json).length;
-		//dom.byId("numRecords").innerHTML = recordsReturned;
-	}
+        //Catch any of the errors that were created from the previous callback functions
+        .catch(function(error) {
+          console.log('One of the promises in the chain was rejected! Message: ', error);
+        });
+
+      // Display the amount of results returned
+      //recordsReturned = Object.keys(json).length;
+      //dom.byId("numRecords").innerHTML = recordsReturned;
+    }
     /**************************************************
      * Create graphics with returned json data
      **************************************************/
@@ -457,38 +456,38 @@ require([
     function createGraphics(response) {
       // raw JSON data
       //var json = response.data;
-	  json = response.data;
-	  recordsReturned = Object.keys(json).length;
-	 
-	  dom.byId("numRecords").innerHTML = recordsReturned;
+      json = response.data;
+      recordsReturned = Object.keys(json).length;
+
+      dom.byId("numRecords").innerHTML = recordsReturned;
 
       // Create an array of Graphics from each JSON feature
-	  // Check to see that the query returned results before trying to create the graphics object JB
-	  if (recordsReturned > 0){
-		return json.map(function(feature, i) {
-			return {
-				type: "point",
-				geometry: new Point({
-				x: json[i].longitude,
-				y: json[i].latitude
-			}),
-			// select only the attributes you care about
-				attributes: {
-				ObjectID: i,
-				address: json[i].address,
-				issueReported: json[i].issue_reported,
-				statusDateTime: json[i].traffic_report_status_date_time,
-				status: json[i].traffic_report_status,
-				latitude: json[i].latitude,
-				longitutde: json[i].longitude
-				}
-			};
-		});
-	  } 
-	  // Reject the promise since no records were returned with the specified query JB
-	  else {
-		  return promiseUtils.reject(new Error("No records to display"));
-	  }
+      // Check to see that the query returned results before trying to create the graphics object JB
+      if (recordsReturned > 0) {
+        return json.map(function(feature, i) {
+          return {
+            type: "point",
+            geometry: new Point({
+              x: json[i].longitude,
+              y: json[i].latitude
+            }),
+            // select only the attributes you care about
+            attributes: {
+              ObjectID: i,
+              address: json[i].address,
+              issueReported: json[i].issue_reported,
+              statusDateTime: json[i].traffic_report_status_date_time,
+              status: json[i].traffic_report_status,
+              latitude: json[i].latitude,
+              longitutde: json[i].longitude
+            }
+          };
+        });
+      }
+      // Reject the promise since no records were returned with the specified query JB
+      else {
+        return promiseUtils.reject(new Error("No records to display"));
+      }
     };
 
     /**************************************************
@@ -504,7 +503,11 @@ require([
         popupTemplate: pTemplate
       });
 
-      try {map.add(trafficFLayer)} catch(error){return };
+      try {
+        map.add(trafficFLayer)
+      } catch (error) {
+        return
+      };
       return trafficFLayer;
     }
 
@@ -528,8 +531,8 @@ require([
 
     /**********************************************************************
      * Dynamically populate Search tab with form elements for incident types
-	 *
-	 * Function author:  JB
+     *
+     * Function author:  JB
      **********************************************************************/
     function populateSearch() {
 
@@ -575,12 +578,12 @@ require([
      * the layer is still part of the map, which means you can access
      * its properties and perform analysis even though it isn't visible.
      *******************************************************************/
-     var roadTrafficStyle = "";
-     var trafficVisible = false;
+    var roadTrafficStyle = "";
+    var trafficVisible = false;
 
-     darkModeToggle = document.getElementById("darkMode");
-     cityLimitsLayerToggle = document.getElementById("cityLimitsLayer");
-     currentTrafficToggle = document.getElementById("currentTraffic");
+    darkModeToggle = document.getElementById("darkMode");
+    cityLimitsLayerToggle = document.getElementById("cityLimitsLayer");
+    currentTrafficToggle = document.getElementById("currentTraffic");
 
 
     if (localStorage.getItem("mode") == "dark") {
@@ -597,13 +600,13 @@ require([
         roadTrafficStyle = "dark";
         darkTrafficTiles.visible = false;
         lightTrafficTiles.visible = false;
-      } else if (darkModeToggle.checked && trafficVisible == true){
+      } else if (darkModeToggle.checked && trafficVisible == true) {
         lightRoads.visible = false;
         darkRoads.visible = true;
         roadTrafficStyle = "dark";
         lightTrafficTiles.visible = false;
         darkTrafficTiles.visible = true;
-      } else if (darkRoads.visible == true && trafficVisible == false){
+      } else if (darkRoads.visible == true && trafficVisible == false) {
         lightRoads.visible = true;
         darkRoads.visible = false;
         roadTrafficStyle = "light";
@@ -633,9 +636,7 @@ require([
         lightTrafficTiles.visible = false;
         darkTrafficTiles.visible = false;
         trafficVisible = false;
-      }
-
-      else if (roadTrafficStyle == "dark") {
+      } else if (roadTrafficStyle == "dark") {
         darkTrafficTiles.visible = true;
         lightTrafficTiles.visible = false;
         trafficVisible = true;
