139cb88c909a6fddc1a25c0c159fe8a5f9ae989b
ipk <ipk1@pm.me>
2018-11-26 20:56:57 -0600

    add demo chart

--- mapping.js
+++ mapping.js
@@ -1057,7 +1057,7 @@ function setDevice() {
         }
       });
     }
-	
+
     /**************************************************
      * Request the  data from data.austin when the
      * view resolves then send it to the
@@ -1081,6 +1081,35 @@ function setDevice() {
 	  on(dom.byId("incidentTypes"), "click", limitSelection);
 
     });
+
+    var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+type: 'doughnut',
+data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+},
+});
 
 
   });
